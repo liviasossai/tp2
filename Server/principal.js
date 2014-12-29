7b3eed30fd06e8df6aded32ff590dec4dd46220d
liviasossai <liviadalfior@gmail.com>
2014-12-29 17:27:06 -0200

    .

--- Server/principal.js
+++ Server/principal.js
@@ -4,9 +4,9 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('Client/'));
 
-/*app.get('/', function(request, response) {
-  //response.send('Hello World!');
-});*/
+app.get('/', function(request, response) {
+  response.sendfile('Client/login.html');
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
